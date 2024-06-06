2040b358367158e78027b2c6ba4c7fd105946d58
Steven Lemon <twynsicle@gmail.com>
2024-06-06 07:27:46 -0700

    styling improvements and more flexible link locations

--- components/SpeedDial.vue
+++ components/SpeedDial.vue
@@ -20,18 +20,27 @@ console.log(config);
   <div class="speeddial">
     <div class="group" v-for="group in parsedConfig.groups">
       <section v-if="group.subgroups != null">
-        <h2>{{ group.name }}</h2>
+        <h2 v-if="group.url != null">
+          <a :href="group.url">{{ group.name }}</a>
+        </h2>
+        <h2 v-else>{{ group.name }}</h2>
         <div class="subgroup" v-for="subgroup in group.subgroups">
-          <h3>{{ subgroup.name }}</h3>
+          <h3 v-if="subgroup.url != null">
+            <a :href="subgroup.url">{{ subgroup.name }}</a>
+          </h3>
+          <h3 v-else>{{ subgroup.name }}</h3>
           <div class="site" v-for="site in subgroup.sites">
-            <button :href="site.url">{{ site.name }}</button>
+            <a class="link" :href="site.url">{{ site.name }}</a>
           </div>
         </div>
       </section>
       <section v-else class="direct-children">
-        <h2>{{ group.name }}</h2>
+        <h2 v-if="group.url != null">
+          <a :href="group.url">{{ group.name }}</a>
+        </h2>
+        <h2 v-else>{{ group.name }}</h2>
         <div class="site" v-for="site in group.sites">
-          <button :href="site.url">{{ site.name }}</button>
+          <a class="link" :href="site.url">{{ site.name }}</a>
         </div>
       </section>
     </div>
@@ -39,64 +48,63 @@ console.log(config);
 </template>
 
 <style scoped lang="scss">
-h2,
-h3 {
-  font-family: 'Montserrat', sans-serif;
-}
-
-button {
-  font-family: 'Raleway', sans-serif;
-}
-
-section {
-  margin: 20px 0;
-}
-
 .speeddial {
-  margin: 100px 10% 0 10%;
-}
+  margin: 150px 10% 0 10%;
 
-.group {
-  display: flex;
-  flex-direction: column;
-}
-
-h2 {
-  font-size: 30px;
-  margin: 10px 0;
-}
-
-.subgroup {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-}
-
-h3 {
-  width: 200px;
-  font-size: 20px;
-  margin: 5px 0;
-}
-
-button {
-  font-size: 20px;
-  padding: 0 10px;
-  background: none;
-  border: none;
-  margin: 0;
-  cursor: pointer;
-  outline: inherit;
-  &:hover {
-    text-decoration: underline;
+  h2,
+  h3 {
+    font-family: 'Montserrat', sans-serif;
+    color: #b6b6b6;
+    a {
+      color: #b6b6b6;
+    }
   }
-}
 
-.direct-children {
+  button {
+    font-family: 'Raleway', sans-serif;
+  }
+
+  section {
+    margin: 20px 0;
+  }
+
+  .group {
+    display: flex;
+    flex-direction: column;
+  }
+
   h2 {
-    width: 200px;
+    font-size: 30px;
+    margin: 10px 0;
+    border: none;
   }
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+
+  .subgroup {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  h3 {
+    width: 200px;
+    font-size: 20px;
+    margin: 5px 0;
+  }
+
+  .link {
+    font-size: 20px;
+    padding: 0 10px;
+    color: #999;
+  }
+
+  .direct-children {
+    h2 {
+      width: 200px;
+    }
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
 }
 </style>
